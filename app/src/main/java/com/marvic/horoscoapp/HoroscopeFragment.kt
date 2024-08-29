02d55bf4e656527ff5649ac9e394ed615c451c16
mfernandezt <pochonumero8@gmail.com>
2024-08-28 21:54:05 -0400

    initial commit

---
+++ app/src/main/java/com/marvic/horoscoapp/HoroscopeFragment.kt
@@ -0,0 +1,23 @@
+package com.marvic.horoscoapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.marvic.horoscoapp.databinding.FragmentHoroscopeBinding
+
+
+class HoroscopeFragment : Fragment() {
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!  //sobreescribe metodo get() y    !! pero no va a ser nulo
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
